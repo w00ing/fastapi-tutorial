adfbd20b39efce46968fdeb2fe670726f4c7029a
Woo-yeong Kim <kimwy1997@gmail.com>
2022-01-04 23:56:54 +0900

    Create routes with query params and body

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import uvicorn
+from typing import Optional
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'hey': 'blog list'}
+@app.get('/blog')
+def index(limit: int = 10, published: bool = False, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {'hey': f'blog published {limit}'}
+    else:
+        return {'hey': f'blog list {limit}'}
+        
 
 @app.get('/blog/unpublished')
 def unpublished():
@@ -14,3 +21,22 @@ def unpublished():
 @app.get('/blog/{id}')
 def show(id: int):
     return {'data': id} 
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int, limit=10):
+    return {'data': [id, limit]} 
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': 'Blog is created'}
+
+    
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=9000)
